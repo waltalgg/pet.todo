9d5a03c0ff2e4561336d3fa48cdc0e0ef8377269
waltalgg <waltalgg@yandex.ru>
2024-09-23 01:24:11 +0300

    1. Частично дописан Header 2. Добавлен header в BlockPattern 3. Обновлен стиль декораций в css для смены текста - hover 4. Немного изменен дизайн на Index'е

--- php/index.php
+++ php/index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
     <?php
-    require_once($_SERVER['DOCUMENT_ROOT'].'\php\debugger.php');
-    require_once($_SERVER['DOCUMENT_ROOT'].'\php\block_pattern.php');
-    require_once($_SERVER['DOCUMENT_ROOT'].'\php\work_with_database.php');  // TODO: Сделать разделение путей на бд, паттерны и основной код index'а
+    require_once($_SERVER['DOCUMENT_ROOT'].'/php/debugger.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/php/block_pattern.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/php/work_with_database.php');  // TODO: Сделать разделение путей на бд, паттерны и основной код index'а
 
     Debugger::WatchError(1);
     ?>
@@ -11,19 +11,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href= "../css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Index</title>
 </head>
 <body>
-
+<header>
+    <?= BlockPattern::ReturnPattern('header') ?>
+</header>
 <div class="container">
-    <div class="row mt-5">
+    <div class="row mt-4 mb-5">
         <div class="col-md-12 text-center">
             <h1>ToDoList</h1>
             <p class="lead"> Заметки и календарь <i>ваших</i> дел</p>
         </div>
     </div>
-    <?= BlockPattern::ReturnPattern('IndexHeadBlock', 6); ?>
+    <?= BlockPattern::ReturnPattern('indexHeadBlock'); ?>
 </div>
 
 </body>
